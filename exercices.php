a3dab8da7f539b3a0aec478695f72b5c47a65df1
Gaelpem <pembelegael@gmail.com>
2024-12-02 23:29:05 +0100

    session

--- exercices.php
+++ exercices.php
@@ -1,16 +1,63 @@
 <?php
+session_start() ; 
+class Devinette{
+         
+   private  $lettre; 
+   
+   public function __construct($lettre = null ){
+    if($lettre !== null){
+        $this->setLettre($lettre); 
+    }
+   }
+   public function setLettre($lettre){
+           $mots =["fraise","jouet"]; 
+             // Convertit en minuscules pour une comparaison insensible à la casse
+           $lettre = strtolower($lettre);
+
+
+           if(!is_string($lettre)){
+            throw new Exception("Le mot n'est pas une chaine de caractére");
+           }
+            
+           $lettre = strtolower($lettre);
+
+          if(!in_array($lettre, $mots)){
+            throw new Exception("Le mot introuvable");
+          } 
+          
+           
+           // Définit le mot seulement s'il est correct
+        $this->lettre = $lettre; 
+   }
+     
+   public function getLettre(){
+    return "Mot correct : ". $this->lettre;
+   }
+}
+
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["lettre"])){
+    $lettres = trim($_POST["lettre"]); 
+    $_SESSION["lettre"]=  $lettres; 
+    if(!empty( $_SESSION["lettre"])){
+       
+        try {        
+        $devinette  = new Devinette();
+        $devinette->setLettre( $_SESSION["lettre"]); 
+        echo $devinette->getLettre(); 
+        }catch (Exception $e){
+            echo $e->getMessage() ; 
+        }
+    }
+
+    }
+
+
+
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
-<body>
-    <div class="rond"></div>
-</body>
-</html>
+<form action="<?= $_SERVER["PHP_SELF"] ; ?>" method="post">
+    <label for="letter">Mot :</label>
+    <input type="text" name="lettre" id="letter" value="" >
+    <button type="submit">Verifier</button>
+</form>
