257b46f6b9098135076253ec4f113bece03bc0ae
Gaelpem <pembelegael@gmail.com>
2024-12-02 10:25:26 +0100

    MotTrver

--- exercices.php
+++ exercices.php
@@ -1,64 +1,64 @@
 <?php
-class   Devinette{
-       
-    private $a ; 
-    public function __construct($a = null){
-        $this->a = $a;
-    }
-    public function verfiMot($a){
-        $mot = "jaune,rouge,rose";
-        
-        $trouverMot = false; 
-            if(is_string(strtolower($a))){
-         
-       
-          
-        $couleurs = explode(',', $mot); 
 
-        $couleurs = array_map('trim', $couleurs);
-        
-        if (in_array(strtolower($a), $couleurs)) {
-            $trouverMot = true;
+
+class Devinette{
+      private $a ; 
+      
+      public function __construct($a = null ){
+        if($a !== null){
+            $this->setA($a);
         }
+      }
+
+      public function setA($a){
+        $mot = "legume"; 
+        $trouve =false ; 
         
-        }
-        if( $trouverMot){
-            $this->a = $a; 
+        if(is_string($a) && strlen($a) === 1){
+            foreach(str_split($mot) as $valeur){
+                if($a == strtolower($valeur)){
+                $trouve = true ; 
+                    break; 
+                }
+            }
+            if($trouve){
+                $this->a = $a ; 
+            }else{
+                throw new Exception("Lettre introuvable !"); 
+            }
+            
         }else{
-            throw new Exception('Couleur incorrect'); 
+            throw new Exception("Vous devez mettre une lettre."); 
         }
-    }
-
- public function affich(){
-    return "Bonne couleur : ".$this->a; 
- }
-
+      }
+      public function getA(){
+        return "Lettre trouvé : " . $this->a; 
+      }
+     
 }
-if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["couleur"])){
-    $couleur = trim($_POST["couleur"]); 
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['letter'])){
+    $letter =  trim($_POST['letter']); 
     
-    if(!empty($couleur)){
+    if(!empty($letter)){
+        
         try{
             $devinette = new Devinette(); 
-            $devinette->verfiMot( $couleur); 
-            echo $devinette->affich(); 
-        }catch (Exception $e){
+            $devinette->setA($letter); 
+            echo $devinette->getA(); 
+        }catch(Exception $e){
             echo $e->getMessage(); 
         }
     }
-
-  
 }
-
-
-
 
 ?>
 
 
-<form action="" method="post">
-    <label for="">Couleur : </label>
-    <input type="text" name="couleur">
-    <input type="submit">
 
+<form action="" method = "post">
+    <label for="lettre">Lettre</label>
+    <input type="text" name="letter" id="lettre">
+    <button type="submit">Vérifier</button>
 </form>
