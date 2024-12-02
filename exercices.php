5c53da4eb43eb1740954d636ea331a3405df0374
Gaelpem <pembelegael@gmail.com>
2024-12-02 11:18:00 +0100

    animation

--- exercices.php
+++ exercices.php
@@ -1,64 +1,16 @@
 <?php
 
-
-class Devinette{
-      private $a ; 
-      
-      public function __construct($a = null ){
-        if($a !== null){
-            $this->setA($a);
-        }
-      }
-
-      public function setA($a){
-        $mot = "legume"; 
-        $trouve =false ; 
-        
-        if(is_string($a) && strlen($a) === 1){
-            foreach(str_split($mot) as $valeur){
-                if($a == strtolower($valeur)){
-                $trouve = true ; 
-                    break; 
-                }
-            }
-            if($trouve){
-                $this->a = $a ; 
-            }else{
-                throw new Exception("Lettre introuvable !"); 
-            }
-            
-        }else{
-            throw new Exception("Vous devez mettre une lettre."); 
-        }
-      }
-      public function getA(){
-        return "Lettre trouvé : " . $this->a; 
-      }
-     
-}
-
-
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['letter'])){
-    $letter =  trim($_POST['letter']); 
-    
-    if(!empty($letter)){
-        
-        try{
-            $devinette = new Devinette(); 
-            $devinette->setA($letter); 
-            echo $devinette->getA(); 
-        }catch(Exception $e){
-            echo $e->getMessage(); 
-        }
-    }
-}
-
 ?>
 
-
-
-<form action="" method = "post">
-    <label for="lettre">Lettre</label>
-    <input type="text" name="letter" id="lettre">
-    <button type="submit">Vérifier</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="rond"></div>
+</body>
+</html>
