7181ea81b73c10386e9ae04e585ce72f1504dbd7
Gaelpem <pembelegael@gmail.com>
2024-12-21 17:20:07 +0100

    session10

--- admin/login-action.php
+++ admin/login-action.php
@@ -25,9 +25,48 @@ class login{
     }
 
 
- public function setNom(string $nom) : void{
-           
+ public function setNom(string $nom) : void
+ {
+           $nom = strtolower($nom); 
+
+           if(iconv_strlen($nom) > 3 &&  iconv_strlen($nom) < 22){
+            $this->nom = $nom ; 
+           }else{
+            throw new Exception(self:ERREUR_NOM); 
+           }
  }
+
+
+
+ public function setEmail(string $email ) : void
+    {
+    if(filter_var($email, FILTER_VALIDE_EMAIL)){
+        $this->email = $email ;  
+    }else{
+            throw new Exception(self:ERREUR_EMAIL); 
+           }
+   }
+
+
+   public function setMdp(string $mdp ) : void
+   {
+                 $caractereSpecials = "$*:/?!=+"; 
+                 
+                 $caractere_Spe = false; 
+
+                foreach(str_split($caractereSpecials) as $caractere){
+                  if(strpos($mdp, $caractere) !== false){
+                    $caractere_Spe = true; 
+                    break; 
+                  }
+                }
+             
+                if(!$caractere_Spe){
+                    throw new Exception(self::ERROR_MDP); 
+                }
+        $this->mdp = $mdp ; 
+
+  }
 
 
 }
